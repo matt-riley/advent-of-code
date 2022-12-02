c6bddb741cdc6bfc234b22659ad4b2a06edb1058
Matt Riley <matt@mattriley.me>
2022-12-02 15:07:45 +0000

    feat: ruby day 1 complete

---
+++ 2022/ruby/day-1/main.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+file = File.read('data.txt')
+
+groups = []
+file.split("\n\n").each do |group|
+  grp_total = group.split("\n").map(&:to_i).sum
+  groups << grp_total
+end
+
+# Part 1
+p groups.max
+
+# Part 2
+groups.sort!.reverse!
+p groups[0..2].sum
